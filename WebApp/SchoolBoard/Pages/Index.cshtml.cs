651268b6711fada4c589499a72d2e420aae78368
Lukas Langrock <Lukas.langrock@outlook.de>
2020-03-15 00:09:21 +0100

    Fill current date in html and respect config

--- WebApp/SchoolBoard/Pages/Index.cshtml.cs
+++ WebApp/SchoolBoard/Pages/Index.cshtml.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace SchoolBoard.Pages
 {
     public class IndexModel : PageModel
     {
+        public string dayOfWeek;
+        public string dayOfMonth;
+        public string dateOfYear;
+
         private readonly ILogger<IndexModel> _logger;
         public Config config;
 
@@ -16,6 +21,26 @@ namespace SchoolBoard.Pages
         public void OnGet()
         {
             config = Configuration.Load();
+            switch (config.Board.Day)
+            {
+                case "Today":
+                    SetDate();
+                    break;
+
+                case "Tomorrow":
+                    SetDate(1);
+                    break;
+            }
+        }
+
+        private void SetDate(int addDays = 0)
+        {
+            DateTime datetime = new DateTime();
+            datetime = DateTime.Now;
+            if (addDays != 0) { datetime = datetime.AddDays(addDays); }
+            dayOfWeek = datetime.ToString("dddd");
+            dayOfMonth = datetime.ToString("dd. MMMM");
+            dateOfYear = datetime.ToShortDateString();
         }
     }
 }
