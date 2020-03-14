c93b6437aeee2a287c5759b0ac37bb1108ab9b97
Lukas Langrock <lukas.langrock@outlook.de>
2020-03-14 11:15:42 +0100

    Update usings

--- WebApp/SchoolBoard/Config.cs
+++ WebApp/SchoolBoard/Config.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SchoolBoard
+﻿namespace SchoolBoard
 {
     public class Config
     {
--- WebApp/SchoolBoard/Configuration.cs
+++ WebApp/SchoolBoard/Configuration.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Text.Json;
-using System.IO;
-using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace SchoolBoard
 {
