156c77d3c6413febec31cba43cba4659e753bddb
Lukas Langrock <lukas.langrock@outlook.de>
2020-05-27 16:27:26 +0200

    Disable settings page

--- WebApp/SchoolBoard/Pages/Settings.cshtml.cs
+++ WebApp/SchoolBoard/Pages/Settings.cshtml.cs
@@ -6,27 +6,9 @@ namespace SchoolBoard
 {
     public class SettingsModel : PageModel
     {
-        private readonly IConfiguration Configuration;
-
-        public SettingsModel(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public void OnGet()
         {
             
-        }
-
-        [BindProperty]
-        public string boardName { get; set; }
-        public IActionResult OnPost()
-        {
-            // save new configuration
-            //TODO: Implement save mechanics using appsettings.json
-
-            // redirect to homepage
-            return Redirect("/");
         }
     }
 }
